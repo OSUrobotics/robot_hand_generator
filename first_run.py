e5318ed0746ee15dff8f10e7e8a908ed3c1c4278
JCampbell9 <campbjos@oregonstate.edu>
2022-03-14 17:42:37 +0000

    finished adding docstrings to the different fuinctions and methods

--- first_run.py
+++ first_run.py
@@ -11,6 +11,12 @@ import shutil
 
 class Setup():
     def __init__(self, input_val, blender_loc= ' '):
+        """Initialize the setup class.
+
+        Args:
+            input_val (int): 0 -> stand alone file structure, 1 -> mojograsp file structure.
+            blender_loc (str, optional): Allows for the path to blender be passed as an argument, if not the script will ask. Defaults to ' '.
+        """
         self.current_directory = os.getcwd()
         
         self.blender_loc = blender_loc
@@ -20,13 +26,22 @@ class Setup():
             self.mojo_grasp_file_strucuture()
 
     def directory_maker(self, location):
+        """Create a new directory
+
+        Args:
+            location (str): absolute path to the directory that you want created
+        """
         if not os.path.isdir(location):
             Path(location).mkdir(parents=True, exist_ok=True)
 
     def mojo_grasp_file_strucuture(self): # adding later
+        """Create the file structure specific to mojo-grasp
+        """
         self.top_directory, _ = os.path.split(self.current_directory)
 
     def stand_alone_file_structure(self):
+        """Create the file structure for stand alone install.
+        """
         directory_dict = {
             'hand_model_output' : f'{self.current_directory}/output/',
             'hand_json_queue': f'{self.current_directory}/hand_json_files/hand_queue_json/',
@@ -36,7 +51,6 @@ class Setup():
         }
 
         for directory_name in directory_dict.keys():
-            # print(directory_name)
             self.directory_maker(directory_dict[directory_name])
 
         directory_dict['src'] = f'{self.current_directory}/src/'
@@ -52,6 +66,11 @@ class Setup():
         self.setup_examples(dict = directory_dict)
 
     def setup_examples(self, dict):
+        """Move the example jsons to the hand_json_queue to be built when main.py is ran.
+
+        Args:
+            dict (dictionary): A dictionary containing needed directories
+        """
         example_loc = dict['hand_examples']
         hand_json_queue = dict['hand_json_queue']
 
@@ -61,8 +80,9 @@ class Setup():
 
 if __name__ == '__main__':
 
-    input_val = sys.argv[1:]
-    if len(input_val) == 2:
+    input_val = sys.argv[1:] 
+
+    if len(input_val) == 2: # checks if the trigger and blender location is passed
         if int(input_val[0]) != 0:
             input_value = 1
             blender_loc = input_val[1]
@@ -71,11 +91,14 @@ if __name__ == '__main__':
             blender_loc = input_val[1]
     elif len(input_val) != 1:
         input_value = 1
+        blender_loc=' '
         
     elif int(input_val[0]) != 0:
         input_value = 1
+        blender_loc=' '
     else:
         input_value = int(input_val[0])
+        blender_loc=' '
         
     Setup(input_val=input_value, blender_loc=blender_loc)
 
--- src/helper_functions.py
+++ src/helper_functions.py
@@ -9,131 +9,151 @@ import json
 import numpy as np
 from pathlib import Path
 
-# class HelperFunctions():
-#     """
-#     Useful functions for creating multiple models in blender
-#     """
-
-    # def __init__(self, current_directory='', export_directory=''):
-    #     pass
-        # self.directory = current_directory
-        # self.export_directory = export_directory
-        # self.sim_dir = ''
-        # self.obj_dir = ''
 
 def blender_make_mesh(verts, faces, mesh_name):
-    edges = []
-    mesh_data = data.meshes.new(mesh_name)
-    mesh_data.from_pydata(verts,edges,faces)
-    bm = bmesh.new()
-    bm.from_mesh(mesh_data)
-    bm.to_mesh(mesh_data)
-    bm.free()
-    mesh_obj = data.objects.new(mesh_data.name, mesh_data)
-    context.collection.objects.link(mesh_obj)
+	"""Create the meshs using blenders tools.
+
+	Args:
+		verts (list): list of tuples, each tuple represents a vertex(x,y,z). 
+		faces (list): list of tuples, each tuple represents a face and contains the index of the vertices used for a face. Follows right hand rule to get face normal.
+		mesh_name (str): The name of the new mesh, make sure it is unique else blender will add .001 to the end if the name already exists.
+	"""
+	edges = [] # Only need edges or faces, we are using faces
+	mesh_data = data.meshes.new(mesh_name)
+	mesh_data.from_pydata(verts,edges,faces)
+	bm = bmesh.new()
+	bm.from_mesh(mesh_data)
+	bm.to_mesh(mesh_data)
+	bm.free()
+	mesh_obj = data.objects.new(mesh_data.name, mesh_data)
+	context.collection.objects.link(mesh_obj)
 
 def directory_maker(location):
-        if not os.path.isdir(location):
-            Path(location).mkdir(parents=True, exist_ok=True)
+	"""Create a new directory.
+
+	Args:
+		location (str): path to new directory that needs to be made.
+	"""
+	if not os.path.isdir(location):
+		Path(location).mkdir(parents=True, exist_ok=True)
 
 def delete_all():
-    """
-    Deletes all objects in the blender enviroment
-    """
-    bpy.ops.object.select_all(action='SELECT')  #deletes everything
-    bpy.ops.object.delete(use_global=False)
+	"""Delete all objects in the blender enviroment."""
+	bpy.ops.object.select_all(action='SELECT')  #deletes everything
+	bpy.ops.object.delete(use_global=False)
 
-def bezier_curve(p0, p1, p2, p3):
+def bezier_curve(p0, p1, p2, p3, resolution=0.01):
+	"""Generate a bezier curve geiven the four points.
+
+	Args:
+		p0 (list): [x,y,z] of the first point
+		p1 (list): [x,y,z] of the second point
+		p2 (list): [x,y,z] of the third point
+		p3 (list): [x,y,z] of the fourth point
+		resolution (float, 0.01): The step size for "time" in the bezier curve, default is 0.01
+
+	Returns:
+		list: list of vectors which each point is a point on the bezier curve.
+	"""
 	points = []
-	for t in np.arange(0.0, 1.01, 0.01):
+	for t in np.arange(0.0, 1 + resolution, resolution):
+		use_t = np.round(t,3)
 		points.append(Vector([
-		(((1 - t) ** 3) * p0[0]) + (3*((1-t)**2) * t * p1[0]) + (3*(1-t) * (t ** 2) *p2[0]) + (t**3 * p3[0]),
-		(((1 - t) ** 3) * p0[1]) + (3*((1-t)**2) * t * p1[1]) + (3*(1-t) * (t ** 2) *p2[1]) + (t**3 * p3[1]),
-		(((1 - t) ** 3) * p0[2]) + (3*((1-t)**2) * t * p1[2]) + (3*(1-t) * (t ** 2) *p2[2]) + (t**3 * p3[2])]))
+		(((1 - use_t) ** 3) * p0[0]) + (3*((1-use_t)**2) * use_t * p1[0]) + (3*(1-use_t) * (use_t ** 2) *p2[0]) + (use_t**3 * p3[0]),
+		(((1 - use_t) ** 3) * p0[1]) + (3*((1-use_t)**2) * use_t * p1[1]) + (3*(1-use_t) * (use_t ** 2) *p2[1]) + (use_t**3 * p3[1]),
+		(((1 - use_t) ** 3) * p0[2]) + (3*((1-use_t)**2) * use_t * p1[2]) + (3*(1-use_t) * (use_t ** 2) *p2[2]) + (use_t**3 * p3[2])]))
 	return points
 
 
 def scale_part(name, scale):
-    """
-    Scales a given object
-    Inputs: name: object name
-            scale: x,y,z values to scale object
-    """
-    bpy.context.view_layer.objects.active = bpy.data.objects[name] 
-    bpy.context.object.scale = scale
+	"""Scale a given object, (not used or tested in new format).
+	
+	Args: 
+		name (str): object name
+		scale (list): x,y,z values to scale object
+	"""
+	bpy.context.view_layer.objects.active = bpy.data.objects[name] 
+	bpy.context.object.scale = scale
 
 def join_parts(names, new_name):
-    """
-    Combine multiple objects together
-    Inputs: names: the names of the objects to be combined
-            new_name: what to name the new object
-    """
+	"""Combine multiple objects together.
 
-    for i in range(len(names) - 1):
-        bpy.data.objects[names[i]].select_set(True)
-    
-    bpy.context.view_layer.objects.active = bpy.data.objects[names[-1]]
-    bpy.ops.object.join()
-    bpy.context.selected_objects[0].name = new_name
+	Args: 
+		names (lsit): list of str, the names of the objects to be combined, the last object will be the cordniate frame the new mesh will have.
+		new_name (str): Name of the new object.
+	"""
+	for i in range(len(names) - 1):
+		bpy.data.objects[names[i]].select_set(True)
+	
+	bpy.context.view_layer.objects.active = bpy.data.objects[names[-1]]
+	bpy.ops.object.join()
+	bpy.context.selected_objects[0].name = new_name
 
 def change_name(old_name, new_name):
-    """
-    change the name of an object
-    Inputs: old_name: name of object you want to change
-            new_name: name you want the object to have
-    """
-    bpy.context.view_layer.objects.active = bpy.data.objects[old_name]
-    bpy.context.selected_objects[0].name = new_name
+	"""Change the name of an object.
+
+	Args: 
+		old_name (str): Current name of object you want to change
+		new_name (str): New name you want the object to have
+	"""
+	bpy.context.view_layer.objects.active = bpy.data.objects[old_name]
+	bpy.context.selected_objects[0].name = new_name
 
 def export_part(name, export_directory):
-    """
-    export the object as an obj file
-    Input: name: name of object you wish to export
-    """
-    name += '.obj'
-    target_file = os.path.join(export_directory, name)
-    bpy.ops.export_scene.obj(filepath=target_file, use_triangles=True, path_mode='COPY', axis_forward="Y", axis_up='Z')
+	"""Export the object as an obj file.
+
+	Arg: 
+		name (str): The absolute path including the name of file object you wish to export.
+	"""
+	name += '.obj'
+	target_file = os.path.join(export_directory, name)
+	bpy.ops.export_scene.obj(filepath=target_file, use_triangles=True, path_mode='COPY', axis_forward="Y", axis_up='Z')
 
 def import_part(file_name, file_location, position=None, rotation=None):
-    
-    # if file_location == None:
-    #     file_location = self.export_directory
-    
-    part_name = file_name
-    file_name += '.obj'
-    # file_name += '.stl'
-    target_file = os.path.join(file_location, file_name)
-    bpy.ops.import_scene.obj(filepath=target_file)
-    
-    num_parts = len((bpy.context.selected_objects))
-    if num_parts > 1:
-        parts_list = []
-        for i in range(num_parts):
-            parts_list.append(bpy.context.selected_objects[i].name)
-        join_parts(parts_list, part_name)
-    else:
-        change_name(bpy.context.selected_objects[-1].name, part_name)
+	"""Import part from a seperate bledner file, (not used or tested in new format).
 
-    if position != None:
-        translate_part(part_name, position)
-    if rotation != None:
-        rotate_part(part_name, rotation)
+	Args:
+		file_name (str): Blender file name
+		file_location (str): path to the blender file
+		position (list, optional): [x,y,z] of the objects location once it is imported. Defaults to None.
+		rotation (list, optional): [r,p,y] of the objects oreintation once it is imported. Defaults to None.
+	"""	
+	part_name = file_name
+	file_name += '.obj'
+	# file_name += '.stl'
+	target_file = os.path.join(file_location, file_name)
+	bpy.ops.import_scene.obj(filepath=target_file)
+	
+	num_parts = len((bpy.context.selected_objects))
+	if num_parts > 1:
+		parts_list = []
+		for i in range(num_parts):
+			parts_list.append(bpy.context.selected_objects[i].name)
+		join_parts(parts_list, part_name)
+	else:
+		change_name(bpy.context.selected_objects[-1].name, part_name)
+
+	if position != None:
+		translate_part(part_name, position)
+	if rotation != None:
+		rotate_part(part_name, rotation)
 
 def rotate_part(part_name, rpy):
+	"""Rotate a given part, (not used or tested in new format).
 
-    bpy.context.view_layer.objects.active = bpy.data.objects[part_name]
-    bpy.context.object.rotation_euler = rpy
+	Args:
+		part_name (str): name of the object
+		rpy (list): [r,p,y] amount to rotate the object by.
+	"""
+	bpy.context.view_layer.objects.active = bpy.data.objects[part_name]
+	bpy.context.object.rotation_euler = rpy
 
 def translate_part(part_name, xyz):
-    bpy.context.view_layer.objects.active = bpy.data.objects[part_name]
-    bpy.context.object.location = xyz
+	"""Translate a given part, (not used or tested in new format).
 
-def set_directories(sim='', obj=''):
-    """
-    Change directories
-    Inputs: sim: simulator directory
-            obj: object directory
-    """
-
-    sim_dir = sim
-    obj_dir = obj
+	Args:
+		part_name (str): name of the part to be translated.
+		xyz (list): [x,y,z] amount to translate the object by.
+	"""
+	bpy.context.view_layer.objects.active = bpy.data.objects[part_name]
+	bpy.context.object.location = xyz
